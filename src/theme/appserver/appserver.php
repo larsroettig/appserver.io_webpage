974483de6cc8544d4bcced484575d445763a3d01
Lars Röttig <roettigl@Larss-MacBook-Pro.local>
2016-02-27 11:06:18 +0100

    Init Commit and Structure

---
+++ src/theme/appserver/appserver.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Grav\Theme\Appserver
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Lars Roettig <lr@@appserver.io>
+ * @copyright 2015 TechDivision GmbH - <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.appserver.io/
+ */
+
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+/**
+ * Class to implement costume theme functions. 
+ *
+ * @author    Lars Roettig <lr@@appserver.io>
+ * @copyright 2015 TechDivision GmbH - <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.appserver.io/
+ * @link      https://github.com/stomp/stomp-spec/blob/master/src/stomp-specification-1.1.md
+ */
+class Appserver extends Theme
+{
+}
